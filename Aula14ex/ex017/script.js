8dc82a8648cd023fd3a591d6d9c52c4e235d9424
Daniel Kommers Contessa <danielkcontessa@gmail.com>
2025-03-23 16:52:04 -0300

    mais um exercício

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,1 +1,7 @@
-alert('Olá')
+function tabuada() {
+var num = document.getElementById('txtn').value
+for (var c = 1; c<=10; c ++) {
+    var x = document.getElementById(`${c}`)
+    x.innerHTML = `${num} x ${c} = ${Number(num)*c} ` 
+}
+}
