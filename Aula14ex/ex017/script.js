15b6d015d010748ee7fe8ca8e46d7d4dcc53565d
Daniel Kommers Contessa <danielkcontessa@gmail.com>
2025-03-23 18:03:16 -0300

    ajuste

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,7 +1,11 @@
 function tabuada() {
 var num = document.getElementById('txtn').value
+if (num.length == 0) {
+    window.alert('Por Favor digite um número!')
+} else {
 for (var c = 1; c<=10; c ++) {
     var x = document.getElementById(`${c}`)
     x.innerHTML = `${num} x ${c} = ${Number(num)*c} ` 
+}
 }
 }
