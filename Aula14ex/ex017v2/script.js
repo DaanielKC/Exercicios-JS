3694e6f7387fc0d2ac3cd255b9825827082ecbe5
Daniel Kommers Contessa <danielkcontessa@gmail.com>
2025-03-23 21:36:58 -0300

    mais ajustes

--- Aula14ex/ex017v2/script.js
+++ Aula14ex/ex017v2/script.js
@@ -1,11 +1,15 @@
 function tabuada() {
     var num = document.getElementById('txtn').value
+    var tab = document.getElementById('tab')
+    var n = Number(num)
     if (num.length == 0) {
         window.alert('Por Favor digite um número!')
     } else {
+        tab.innerHTML = ''
     for (var c = 1; c<=10; c ++) {
-        var a = document.createElement('option') 
-        a.text = `${Number(num)} x ${c} = ${Number(num)*c}` 
+        var item = document.createElement('option') 
+        item.text = `${n} x ${c} = ${n*c} ` 
+        tab.appendChild(item)
     }
     }
 }
